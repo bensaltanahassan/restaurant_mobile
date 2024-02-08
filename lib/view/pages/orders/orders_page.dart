1c170da23b0909ecf44e850787c69d3f980336a2
bensaltana hassan <bensaltana10@gmail.com>
2024-02-08 11:23:12 +0100

    add orders page

---
+++ lib/view/pages/orders/orders_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:restaurant_mobile/core/constant/colors.dart';
+import 'package:restaurant_mobile/core/constant/constants.dart';
+import 'package:restaurant_mobile/core/constant/routes.dart';
+
+class OrdersPage extends StatelessWidget {
+  const OrdersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Orders'),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: AppColors.whiteColor,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: ListView.separated(
+        padding: const EdgeInsets.symmetric(
+            horizontal: AppDimensions.mainPadding,
+            vertical: AppDimensions.mainPadding / 2),
+        itemCount: 20,
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider(
+            height: 30,
+            color: AppColors.greyColor,
+          );
+        },
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: const Text('Order #1234'),
+            subtitle: const Text('Order Date: 12/12/2021'),
+            trailing: const Text('Total: \$100.00'),
+            onTap: () {
+              Get.toNamed(AppRoutes.orderdetails);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/view/pages/profile/profile_page.dart
+++ lib/view/pages/profile/profile_page.dart
@@ -12,71 +12,75 @@ class ProfilePage extends StatelessWidget {
     final controller = Get.put(ProfileController());
     return Padding(
       padding: const EdgeInsets.only(left: 20, right: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text("Profile",
-              style: TextStyle(
-                fontSize: 30,
-                height: 1,
-                fontWeight: FontWeight.bold,
-                color: AppColors.whiteColor,
-              )),
-          const SizedBox(height: 20),
-          ListTile(
-            //disable shap
-            shape: const OutlineInputBorder(borderSide: BorderSide.none),
-            leading: ClipRRect(
-              borderRadius: BorderRadius.circular(30),
-              child: Image.asset(AppImageAsset.hassan,
-                  fit: BoxFit.cover, width: 60, height: 60),
+      child: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Profile",
+                style: TextStyle(
+                  fontSize: 30,
+                  height: 1,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.whiteColor,
+                )),
+            const SizedBox(height: 20),
+            ListTile(
+              //disable shap
+              shape: const OutlineInputBorder(borderSide: BorderSide.none),
+              leading: ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Image.asset(AppImageAsset.hassan,
+                    fit: BoxFit.cover, width: 60, height: 60),
+              ),
+              title: const Text("BENSALTANA HASSAN"),
+              subtitle: const Text("Member since 2021"),
             ),
-            title: const Text("BENSALTANA HASSAN"),
-            subtitle: const Text("Member since 2021"),
-          ),
-          const SizedBox(height: 40),
-          ListTile(
-            title: const Text("User Settings"),
-            leading: const Icon(Icons.settings),
-            trailing: const Icon(Icons.arrow_forward_ios_rounded),
-            onTap: () => controller.goToUserSettings(),
-          ),
-          const SizedBox(height: 20),
-          ListTile(
-            leading: const Icon(Icons.lock),
-            title: const Text("Change Password"),
-            trailing: const Icon(Icons.arrow_forward_ios_rounded),
-            onTap: () => controller.goToChangePassword(),
-          ),
-          const SizedBox(height: 20),
-          ListTile(
-            leading: const Icon(Icons.favorite),
-            title: const Text("Favorite"),
-            trailing: const Icon(Icons.arrow_forward_ios_rounded),
-            onTap: () => controller.goToFavorite(),
-          ),
-          const SizedBox(height: 20),
-          ListTile(
-            title: const Text("Recent Orders"),
-            leading: const Icon(Icons.shopping_cart),
-            trailing: const Icon(Icons.arrow_forward_ios_rounded),
-            onTap: () => controller.goToRecentOrders(),
-          ),
-          const SizedBox(height: 20),
-          ListTile(
-            title: const Text("Notifications"),
-            leading: const Icon(Icons.notifications),
-            trailing: const Icon(Icons.arrow_forward_ios_rounded),
-            onTap: () => controller.goToNotifications(),
-          ),
-          const SizedBox(height: 20),
-          ListTile(
-            leading: const Icon(Icons.logout),
-            title: const Text("Logout"),
-            trailing: const Icon(Icons.arrow_forward_ios_rounded),
-            onTap: controller.logout,
-          ),
-        ],
+            const SizedBox(height: 40),
+            ListTile(
+              title: const Text("User Settings"),
+              leading: const Icon(Icons.settings),
+              trailing: const Icon(Icons.arrow_forward_ios_rounded),
+              onTap: () => controller.goToUserSettings(),
+            ),
+            const SizedBox(height: 20),
+            ListTile(
+              leading: const Icon(Icons.lock),
+              title: const Text("Change Password"),
+              trailing: const Icon(Icons.arrow_forward_ios_rounded),
+              onTap: () => controller.goToChangePassword(),
+            ),
+            const SizedBox(height: 20),
+            ListTile(
+              leading: const Icon(Icons.favorite),
+              title: const Text("Favorite"),
+              trailing: const Icon(Icons.arrow_forward_ios_rounded),
+              onTap: () => controller.goToFavorite(),
+            ),
+            const SizedBox(height: 20),
+            ListTile(
+              title: const Text("Recent Orders"),
+              leading: const Icon(Icons.shopping_cart),
+              trailing: const Icon(Icons.arrow_forward_ios_rounded),
+              onTap: () => controller.goToRecentOrders(),
+            ),
+            const SizedBox(height: 20),
+            ListTile(
+              title: const Text("Notifications"),
+              leading: const Icon(Icons.notifications),
+              trailing: const Icon(Icons.arrow_forward_ios_rounded),
+              onTap: () => controller.goToNotifications(),
+            ),
+            const SizedBox(height: 20),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text("Logout"),
+              trailing: const Icon(Icons.arrow_forward_ios_rounded),
+              onTap: controller.logout,
+            ),
+            const SizedBox(height: 40)
+          ],
+        ),
       ),
     );
   }
